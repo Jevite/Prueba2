d59297179acd5e4a57845a5957c1aebbe611282e
Jevit <adjavier@uninorte.edu.co>
2021-04-14 16:49:18 -0500

    hash

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Register from "./Components/Register/Register";
 import Stats from "./Components/Stats/Stats";
 import History from "./Components/History/Main";
 import { CssBaseline, CircularProgress } from "@material-ui/core";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import UserContext from "./Context/context";
 import Axios from "axios";
 
@@ -16,12 +16,37 @@ function App(props) {
     user: undefined,
   });
 
+  useEffect(() => {
+    const checkLogged = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+
+      const tokenR = await Axios.post(
+        "http://ec2-18-218-144-116.us-east-2.compute.amazonaws.com/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenR.data) {
+        const userRes = await Axios.get("http://ec2-18-218-144-116.us-east-2.compute.amazonaws.com/users/", {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+    checkLogged();
+  }, []);
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
       <div>
         <CssBaseline />
-        <Router basename="Medicine">
+        <Router>
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/registro" component={Register} />
